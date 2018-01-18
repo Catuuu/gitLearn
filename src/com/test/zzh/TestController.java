644b6a39fa5d9b10805070e5bca093292767bf5c
Catuuu <214197411@qq.com>
2018-01-18 14:02:09 +0800

    修改项目配置文件

--- src/com/test/zzh/TestController.java
+++ src/com/test/zzh/TestController.java
@@ -2,15 +2,17 @@ package com.test.zzh;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
 
 /**
  * Created by 二狗子 on 2018/1/16 0016.
  */
 @Controller
+@RequestMapping(value = "hello",method = RequestMethod.GET)
 public class TestController {
-    @RequestMapping("/hello")
+    @RequestMapping("hello")
     public String helloWorld(){
-        return "test/hello.jsp";
+        return "test/hello";
     }
 }
