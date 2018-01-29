427dc36a479084f4f74c4523b8d4d550cc5c488b
Catuuu <214197411@qq.com>
2018-01-29 14:36:42 +0800

    单元测试类

--- src/com/test/zzh/TestController.java
+++ src/com/test/zzh/TestController.java
@@ -15,4 +15,54 @@ public class TestController {
     public String helloWorld(){
         return "test/hello";
     }
+
+    public static void main(String[] args) {
+//        System.out.println("打印main方法中的输入参数！");
+//        for(int i=0;i<args.length;i++){
+//            System.out.println(args[i]);
+//        }
+        //下面是完整的程序,代码如下。(选择排序)
+        int[] Interray={12,1145,68,43,40,57,3,20};
+        int keyValue;
+        int index;
+        int temp;
+        System.out.println("排序前的数组");
+        for(int i=0; i<Interray.length; i++){
+            System.out.println(Interray[i]+"");
+            System.out.println("");
+        }
+        for (int i = 0; i <Interray.length; i++) {
+            index = i;
+            keyValue = Interray[i];
+            for (int j = i; j <Interray.length ; j++) {
+                if (keyValue>Interray[j]){
+                    index = j;
+                    keyValue = Interray[j];
+                }
+            }
+            temp = Interray[i];
+            Interray[i]=keyValue;
+            Interray[index] = temp;
+        }
+        System.out.println("排序后的数组");
+        for (int i = 0; i < Interray.length; i++) {
+            System.out.println(Interray[i]);
+        }
+
+        //冒泡排序
+        for (int i = 0; i <Interray.length ; i++) {
+            for (int j = i; j <Interray.length ; j++) {
+                if (Interray[j]<Interray[i]){
+                    temp = Interray[i];
+                    Interray[i] = Interray[j];
+                    Interray[j] = temp;
+                }
+            }
+        }
+        System.out.println("冒泡排序后的数组");
+        for (int i = 0; i <Interray.length ; i++) {
+            System.out.println(Interray[i]);
+        }
+
+    }
 }
